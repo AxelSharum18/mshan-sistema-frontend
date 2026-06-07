bb86337711d4c7fc720cc5a99129910c3826c3b3
AxelSharum18 <axelshanr17@gmail.com>
2026-06-07 11:42:39 -0500

    fix/icon

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Pencil, Trash2 } from 'lucide-react';
 
 const DataTable = ({
   columns,
